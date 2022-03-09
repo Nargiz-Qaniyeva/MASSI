7dd4fa4b59916f9dd5055ee713c7269baa70be97
Nargiz-Qaniyeva <tu7lyw21z@code.edu.az>
2022-03-10 01:50:40 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "CODE";
+            string reverse = ""; //massivi tersden oxutma
+            int length = str.Length - 1;
+
+            for (int i = 0; i < length; i++) 
+            {
+                while (length >= 0) 
+                {
+                    reverse += str[length];
+                    length-=1;   
+                }
+               
+            }
+            Console.WriteLine(reverse);
+
+
+        }
+    }
+}
